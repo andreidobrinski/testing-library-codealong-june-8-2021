f28baf8541550ab9daeab5abcac22c621e44f61d
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-06-08 19:58:28 -0400

    add first test

--- src/form.test.tsx
+++ src/form.test.tsx
@@ -9,22 +9,61 @@ import * as Fetch from "./fake-fetch";
 // fetch
 // date
 
-const arrange = async () => {
+let getItemSpy;
+let fakeFetch = jest.spyOn(Fetch, "fakeFetch");
+
+const arrange = async ({ responseMock }) => {
+  fakeFetch.mockImplementation(() => Promise.resolve(responseMock));
   await act(async () => {
     render(<Form />);
   });
 };
 
 describe("Form", () => {
+  beforeEach(() => {
+    global.fakeThirdPartyJSLibrary = "hello world";
+    getItemSpy = jest
+      .spyOn(global.Storage.prototype, "getItem")
+      .mockReturnValue("hello");
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  afterAll(() => {
+    delete global.fakeThirdPartyJSLibrary;
+  });
+
   it("should render, fetch data from the api, localstorage and globals and display the data", async () => {
+    const responseMock = {
+      date: "2020-02-01",
+      text: "#invalid",
+    };
+
+    await arrange({ responseMock });
+
+    expect(
+      screen.getByRole("heading", {
+        name: "hello world",
+      }).textContent
+    ).toEqual("hello world");
+
+    expect(
+      (screen.getByLabelText(/enter a date:/i) as HTMLInputElement).value
+    ).toEqual("2020-02-01");
+    expect(
+      (screen.getByRole("textbox", {
+        name: /enter good text/i,
+      }) as HTMLInputElement).value
+    ).toEqual("#invalid");
+  });
+
+  xit("should display errors with invalid data and prevent form submit", async () => {
     await arrange();
   });
 
-  it("should display errors with invalid data and prevent form submit", async () => {
-    await arrange();
-  });
-
-  it("should display a success message with valid data on form submit", async () => {
+  xit("should display a success message with valid data on form submit", async () => {
     await arrange();
   });
 });
